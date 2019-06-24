5eb9a535b0b0e1269aed33f92835cb54a1b83e74
apexof <apexof@gmail.com>
2019-06-24 18:03:30 +0300

    закончил process

--- src/main.js
+++ src/main.js
@@ -30,9 +30,56 @@ $(document).ready(() => {
     $("#show-all-projects").click(() => {
         $(".projects__list-el-container:nth-child(10)").toggle("medium");
     });
-    $("#show-all-thanks").click(() => {
-        if (document.documentElement.clientWidth < 901) {
-            $(".review__thank-list a:nth-child(n + 9)").toggle("medium");
-        }
+    $(".process__arrow").mouseenter(() => {
+        $(".process__list-el:nth-child(n+2)").css("display", "flex");
+        $(".process__arrow").hide();
+    });
+
+    $(".review__thank-list").slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        centerPadding: 0,
+        lazyLoad: "ondemand",
+        adaptiveHeight: false,
+        centerMode: false,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            }
+        ]
+    });
+    $(".process__list").slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        arrows: true,
+        adaptiveHeight: true,
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 3000,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 651,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $(window).on("resize orientationchange", () => {
+        $(".process__list").slick("resize");
     });
 });
