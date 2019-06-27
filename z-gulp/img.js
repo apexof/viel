d096f7e7904235e45fda03d2901cf42c5e8a3e40
apexof <apexof@gmail.com>
2019-06-27 16:19:13 +0300

    save

--- z-gulp/img.js
+++ z-gulp/img.js
@@ -6,6 +6,7 @@ const Mozjpeg = require("imagemin-mozjpeg");
 const Gifsicle = require("imagemin-gifsicle");
 const pngquant = require("imagemin-pngquant");
 const svgo = require("imagemin-svgo");
+const webp = require("imagemin-webp");
 
 task("minify", () => src("./src/**/*.{png,jpg,svg,gif}", { since: lastRun("minify") })
     .pipe($.rename(path => path.dirname = ""))
@@ -23,6 +24,21 @@ task("minify", () => src("./src/**/*.{png,jpg,svg,gif}", { since: lastRun("minif
     }))
     .pipe($.debug({ title: "minify" }))
     .pipe(dest("./build/img")));
+
+task("webp", () => src("./src/**/*.{png,jpg}", { since: lastRun("webp") })
+    .pipe($.rename(path => path.dirname = ""))
+    .pipe($.newer({
+        dest: "./build/img/webp",
+        ext: ".jpg"
+    }))
+    .pipe($.imagemin([
+        webp({
+            quality: 85,
+            preset: "photo"
+        })]))
+    .pipe($.debug({ title: "webp" }))
+    .pipe(dest("./build/img/webp")));
+
 
 task("sqip", () => src("./build/img/*.{png,jpg}", { since: lastRun("sqip") })
     .pipe($.rename(path => path.dirname = ""))
